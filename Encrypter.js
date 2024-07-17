8e4a85021d883ae078088a8c12f4d97fefffe6d0
law <161666444+lawlol@users.noreply.github.com>
2024-07-16 23:29:43 -0400

    Create Encrypter.js

---
+++ Encrypter.js
@@ -0,0 +1,24 @@
+CryptoJS = require('crypto-js');
+fs = require('fs');
+
+function EncryptCode(code) {
+  const key = 'key here';
+  const encrypted = CryptoJS.AES.encrypt(code, key).toString();
+  return encrypted;
+}
+
+function DecryptCode(encryptedCode) {
+  const key = 'key here';
+  const decrypted = CryptoJS.AES.decrypt(encryptedCode, key).toString(CryptoJS.enc.Utf8);
+  return decrypted;
+}
+
+function RunEncryptedCode(encryptedCode) {
+  const decryptedCode = DecryptCode(encryptedCode);
+  eval(decryptedCode);
+}
+
+function ReadCodeFromFile(filepath) {
+  const code = fs.readFileSync(filepath, 'utf-8');
+  return code;
+}
